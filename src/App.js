f046e2dc19a3346ed1d454cd1219545315cfa93e
Shweta Sharma <sshweta437@gmail.com>
2023-08-06 05:58:17 +0530

    Initial Commit, Components, JSX, List rendering, Conditionals

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const title = "Blog Post";
+	const body = "This is my blog post";
+	const comments = [
+		{ id: 1, text: "Comment One" },
+		{ id: 2, text: "Comment two" },
+		{ id: 3, text: "Comment three" },
+	];
+	return (
+		<div className="conteiner">
+			<h1>{title}</h1>
+			<p>{body}</p>
+			<div className="comments">
+				<h3>Comments ({comments.length})</h3>
+				<ul>
+					{comments.map((comment, index) => (
+						<li key={index}>{comment.text}</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
 }
-
 export default App;
